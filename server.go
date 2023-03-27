97e047097a56c2c71d61641b135688ff5c23f8f4
Frank Jahn <fjahn78@gmail.com>
2023-03-27 07:38:50 +0000

    Test case different player

--- server.go
+++ server.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-
-func PlayerServer(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "20")
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	switch player {
+	case "Pepper":
+		fmt.Fprint(w, "20")
+	case "Floyd":
+		fmt.Fprint(w, "10")
+	}
 }
