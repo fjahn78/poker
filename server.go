ffe6e4cc43bcc95bca8b6499cbccee0b8a2d9402
Frank Jahn <fjahn78@gmail.com>
2023-03-28 07:43:54 +0000

    Refactor

--- server.go
+++ server.go
@@ -15,6 +15,7 @@ type Player struct {
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
+	GetLeague() []Player
 }
 
 type PlayerServer struct {
@@ -38,18 +39,18 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
-	err := json.NewEncoder(w).Encode(p.getLeagueTable())
+	err := json.NewEncoder(w).Encode(p.store.GetLeague())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 	w.WriteHeader(http.StatusOK)
 }
 
-func (p *PlayerServer) getLeagueTable() []Player {
-	return []Player{
-		{"Chris", 20},
-	}
-}
+// func (p *PlayerServer) getLeagueTable() []Player {
+// 	return []Player{
+// 		{"Chris", 20},
+// 	}
+// }
 
 func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
