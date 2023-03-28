0a6237a7286660488d7490e4fe6cf336c823b775
Frank Jahn <fjahn78@gmail.com>
2023-03-28 14:07:05 +0000

    Update main

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
-// type InMemoryPlayerStore struct{}
-
-// func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-// 	return 123
-// }
-
-// func (i *InMemoryPlayerStore) RecordWin(name string) {}
+const dbFileName = "game.db.json"
 
 func main() {
-	// handler := http.HandlerFunc(PlayerServer)
-	server := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":5000", server))
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
