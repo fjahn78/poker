88351a5a9c316bb65a05c70f5e7d7a0ee8be3238
Frank Jahn <fjahn78@gmail.com>
2023-03-28 08:31:35 +0000

    Update integration test

--- server_integration_test.go
+++ server_integration_test.go
@@ -15,9 +15,27 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newWinPostRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newWinPostRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
+	t.Run("get score", func(t *testing.T) {
 
-	assertResponseBody(t, response.Body.String(), "3")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
+
+		assertResponseBody(t, response.Body.String(), "3")
+	})
+
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+		assertStatus(t, response.Code, http.StatusOK)
+
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{
+				Name: "Pepper",
+				Wins: 3,
+			},
+		}
+		assertLeague(t, got, want)
+	})
 }
