661482aea2bc859ac1d08c47ac268d4bc025878e
Frank Jahn <fjahn78@gmail.com>
2023-03-29 09:26:25 +0000

    refactor constructor

--- file_system_store.go
+++ file_system_store.go
@@ -13,9 +13,11 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	if _, err := file.Seek(0, 0); err != nil {
-		log.Fatal(err)
+	err := initializePlayerDBFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("problem initializing player db file, %v", err)
 	}
+
 	league, err := NewLeague(file)
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
@@ -24,6 +26,21 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	if _, err := file.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+	if info.Size() == 0 {
+		_, _ = file.Write([]byte("[]"))
+		_, _ = file.Seek(0, 0)
+	}
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
