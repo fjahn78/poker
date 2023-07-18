aeb8a195fe98aacb13586c4360ab91652a8c8c3f
Frank Jahn <frank.jahn@cgm.com>
2023-07-18 10:58:46 +0200

    fix linting

--- league.go
+++ league.go
@@ -12,7 +12,7 @@ func NewLeague(rdr io.Reader) ([]Player, error) {
 	var league []Player
 	err := json.NewDecoder(rdr).Decode(&league)
 	if err != nil {
-		err = fmt.Errorf("Unable to decode json: %q", err)
+		err = fmt.Errorf("unable to decode json: %q", err)
 	}
 	return league, err
 }
